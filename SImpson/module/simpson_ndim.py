8f041c12cc7f0946125aaaf0be8e1ce7ac839c72
Elio <rosarioelio.profumo@cea.fr>
2019-08-17 12:23:46 +0200

    refactoring test

--- SImpson/module/simpson_ndim.py
+++ SImpson/module/simpson_ndim.py
@@ -1,32 +1,35 @@
-#routine for Simpson integration on a hyper-triangular grid
+# routine for Simpson integration on a hyper-triangular grid
 
-def f(length, conf, tmax):
+
+def func(length, conf, tmax):
     if length == 1:
-        return Simpson(conf[0], tmax)
+        return simpson(conf[0], tmax)
     else:
         for i in xrange(length):
             l = conf[-1]
             k = conf[-2]
-            if k<tmax-2:
-                return Simpson(l - k, tmax-k)*f(length-1,conf[:-1], tmax)
+            if k < tmax-2:
+                return simpson(l - k, tmax-k)*func(length-1, conf[:-1], tmax)
             else:
-                return weight(l, k, tmax) * f(length-1,conf[:-1], tmax)
+                return weight(l, k, tmax) * func(length-1, conf[:-1], tmax)
+
 
 def weight(l, k, tmax):
     if k == tmax:
-        w =0.
+        w = 0.
     elif k == tmax-1:
-        w= 1/2.
-    elif k==tmax-2:
-        if l==tmax:
-            w= 1/3.
-        elif l==tmax-1:
-            w=4/3.
+        w = 1/2.
+    elif k == tmax-2:
+        if l == tmax:
+            w = 1/3.
+        elif l == tmax-1:
+            w = 4/3.
         else:
-            w=1/3.
+            w = 1/3.
     return w
 
-def Simpson(l, k):
+
+def simpson(l, k):
     if k % 2 == 0:
         if (l == 0 or l == k):
             weight = 1/3.
@@ -39,11 +42,10 @@ def Simpson(l, k):
             weight = 1/3.
         elif l == k:
             weight = 1/2.
-        elif l == k -1:
+        elif l == k - 1:
             weight = 5/6.
         elif l % 2 != 0:
             weight = 4/3.
         else:
             weight = 2/3.
     return weight
-
---
+++ SImpson/test/test_simpson_ndim.py
@@ -0,0 +1,135 @@
+import unittest
+import numpy as np
+from module.simpson_ndim import func
+
+
+def function1(x):
+    return x**4
+
+
+def function2(x, y):
+    return x**2 * y + x * y**3
+
+
+def function3(x, y, z):
+    return x**2 * y * z**3
+
+
+def function4(x, y, z):
+    return np.exp(-(x+y+z)) * np.cos(x+y+z)
+
+
+def function5(y, z, omega):
+    return (y+z) * np.cos(omega*(y+z))
+
+
+def function6(x, y, z, omega):
+    return (x+y+z) * np.cos(omega*(x+y+z))
+
+
+epsilon = 1e-2
+
+
+class TestSimpsonNdim(unittest.TestCase):
+
+    def test_triangularsimpson_one_var(self):
+
+        # polynomial 1 variables
+        length = 1
+        dt = 1e-3
+        tmreal = 5
+        a = 0
+        nt = int(tmreal/dt)
+        for i in xrange(nt+1):
+            a += function1(i*dt) * func(length,
+                                        np.array([i]), nt)
+        res1 = a*dt
+        res2 = tmreal**5/5.
+
+        assert abs((res1-res2)/res2) < epsilon
+
+    def test_triangularsimpson_two_var(self):
+
+        # polynomial 2 variables
+        length = 2
+        dt = 0.01
+        tmreal = 5
+        b = 0
+        nt = int(tmreal/dt)
+        for i in xrange(nt+1):
+            for j in xrange(i, nt+1):
+                b += function2(i*dt, j*dt) * \
+                    func(length, np.array([i, j]), nt)
+        res1 = b*dt**2
+        res2 = tmreal**5*(1/6.-1/10.)+tmreal**6*(1/8.-1/24.)
+        assert abs((res1-res2)/res2) < epsilon
+
+    def test_triangularsimpson_three_var(self):
+
+        # polynomial 3 variables
+        length = 3
+        dt = 0.1
+        tmreal = 5
+        c = 0
+        nt = int(tmreal/dt)
+        for i in xrange(nt+1):
+            for j in xrange(i, nt+1):
+                for k in xrange(j, nt+1):
+                    c += function3(i*dt, j*dt, k*dt) * \
+                        func(length, [i, j, k], nt)
+        res1 = c*dt**3
+        res2 = tmreal**9*(1/24. - 1/72. - 1/40. + 1/216.)
+        assert abs((res1-res2)/res2) < epsilon
+
+    def test_triangularsimpson_oscillating(self):
+
+        # oscillating decaying functions
+        length = 3
+        dt = 0.05
+        tmax = 15
+        nt = int(tmax/dt)
+        d = 0
+        for i in xrange(nt+1):
+            for j in xrange(i, nt+1):
+                for k in xrange(j, nt+1):
+                    d += function4(i*dt, j*dt, k*dt) * \
+                        func(length, [i, j, k], nt)
+        res1 = d*dt**3
+        res2 = 0.25 * (-1/4. * np.exp(-3*tmax) * (np.sin(3*tmax)+np.cos(3*tmax))+1/6.*np.exp(-3*tmax)
+                       * (np.sin(3*tmax)+np.cos(3*tmax))+1/4.*np.exp(-tmax)*(np.sin(tmax)+np.cos(tmax))-1/6.)
+        assert abs((res1-res2)/res2) < epsilon
+
+        length = 2
+        dt = 0.02
+        tmax = 5
+        omega = 5
+        nt = int(tmax/dt)
+        e = 0
+        for i in xrange(nt+1):
+            for j in xrange(i, nt+1):
+                e += function5(i*dt, j*dt, omega) * \
+                    func(length, [i, j], nt)
+        res1 = e*dt**2
+        res2 = (np.sin(2*omega*tmax)-tmax*omega*np.cos(2*omega*tmax) -
+                2*np.sin(omega*tmax)+omega*tmax*np.cos(omega*tmax))/omega**3
+        assert abs((res1-res2)/res2) < epsilon
+
+        length = 3
+        dt = 0.05
+        tmax = 10
+        nt = int(tmax/dt)
+        d = 0
+        omega = 5
+        for i in xrange(nt+1):
+            for j in xrange(i, nt+1):
+                for k in xrange(j, nt+1):
+                    d += function6(i*dt, j*dt, k*dt, omega) * \
+                        func(length, [i, j, k], nt)
+        res1 = d*dt**3
+        res2 = 1/(2.*omega**4) * (-omega*tmax*np.sin(3*omega*tmax)-np.cos(3*omega*tmax)+2*tmax*omega *
+                                  np.sin(2*omega*tmax)-omega*tmax*np.sin(omega*tmax)+3*np.cos(2*omega*tmax)-3*np.cos(omega*tmax)+1)
+        assert abs((res1-res2)/res2) < epsilon
+
+
+if __name__ == "__main__":
+    unittest.main()
